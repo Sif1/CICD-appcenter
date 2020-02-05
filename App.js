8c28becb0d7e4076b099119a2518723711daadea
gustavo souto <gustavo.souto@rethink.dev>
2020-02-05 13:37:37 -0300

    inflation formulas and basic layout

--- App.js
+++ App.js
@@ -10,16 +10,54 @@
 import React from 'react';
 import Crashes from 'appcenter-crashes';
 import Analytics from 'appcenter-analytics';
-import {Button, View, StyleSheet} from 'react-native';
+import {Button, View, StyleSheet, Text, TextInput} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      inflationRate: 0.0,
+      riskFreeRate: 0.0,
+      amount: 0.0,
+      timeInYears: 1,
+      afterInflation: 0.0,
+      atRiskFree: 0.0,
+      atRiskFreeAfterInflation: 0.0,
+      difference: 0,
+    };
   }
 
   componentDidMount() {
     this.checkPreviousSession();
+  }
+
+  calculateInflationImpact(value, inflationRate, time) {
+    return value / Math.pow(1 + inflationRate, time);
+  }
+
+  calculate() {
+    const afterInflation = this.calculateInflationImpact(
+      this.state.amount,
+      this.state.inflationRate / 100,
+      this.state.timeInYears,
+    );
+    const atRiskFree =
+      this.state.amount *
+      Math.pow(1 + this.state.riskFreeRate / 100, this.state.timeInYears);
+    const atRiskFreeAfterInflation = this.calculateInflationImpact(
+      atRiskFree,
+      this.state.inflationRate / 100,
+      this.state.timeInYears,
+    );
+    const difference = atRiskFreeAfterInflation - afterInflation;
+
+    this.setState({
+      afterInflation,
+      atRiskFree,
+      atRiskFreeAfterInflation,
+      difference,
+    });
   }
 
   checkPreviousSession = async () => {
@@ -35,9 +73,59 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Button title="Crash" onPress={() => Crashes.generateTestCrash()} />
         <Button
-          title="Calculate Inflation"
-          onPress={() => Analytics.trackEvent('INFLATION CALC')}
+          title="Track Event"
+          onPress={() => Analytics.trackEvent('Event Tracked - click')}
         />
+        <TextInput
+          placeholder="Current inflation rate"
+          style={styles.textBox}
+          keyboardType="decimal-pad"
+          onChangeText={inflationRate => this.setState({inflationRate})}
+        />
+        <TextInput
+          placeholder="Current risk free rate"
+          style={styles.textBox}
+          keyboardType="decimal-pad"
+          onChangeText={riskFreeRate => this.setState({riskFreeRate})}
+        />
+        <TextInput
+          placeholder="Amount you want to save"
+          style={styles.textBox}
+          keyboardType="decimal-pad"
+          onChangeText={amount => this.setState({amount})}
+        />
+        <TextInput
+          placeholder="For how long (in years) will you save?"
+          style={styles.textBox}
+          keyboardType="decimal-pad"
+          onChangeText={timeInYears => this.setState({timeInYears})}
+        />
+        <Button
+          title="Calculate inflation"
+          onPress={() => {
+            this.calculate();
+            Analytics.trackEvent('calculate_inflation', {
+              Internet: 'WiFi',
+              GPS: 'Off',
+            });
+          }}
+        />
+        <Text style={styles.label}>
+          {this.state.timeInYears} years from now you will still have $
+          {this.state.amount} but it will only be worth $
+          {this.state.afterInflation}.
+        </Text>
+        <Text style={styles.label}>
+          But if you invest it at a risk free rate you will have $
+          {this.state.atRiskFree}.
+        </Text>
+        <Text style={styles.label}>
+          Which will be worth ${this.state.atRiskFreeAfterInflation} after
+          inflation.
+        </Text>
+        <Text style={styles.label}>
+          A difference of: ${this.state.difference}.
+        </Text>
       </View>
     );
   }
@@ -46,8 +134,51 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     marginTop: 40,
-    marginLeft: 20,
-    height: 200,
-    width: 200,
+    marginHorizontal: 16,
+  },
+  label: {
+    marginTop: 10,
+  },
+  textBox: {
+    height: 30,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 10,
+  },
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
   },
 });
