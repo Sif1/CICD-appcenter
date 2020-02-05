375357c5aa75d2077962f56c83d7eef4c959a1a0
gustavo souto <gustavo.souto@rethink.dev>
2020-02-05 13:04:48 -0300

    event tracking

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-alert */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -8,13 +9,35 @@
 
 import React from 'react';
 import Crashes from 'appcenter-crashes';
+import Analytics from 'appcenter-analytics';
 import {Button, View, StyleSheet} from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.checkPreviousSession();
+  }
+
+  checkPreviousSession = async () => {
+    //check if the app has crashed in the last active session
+    const didCrash = await Crashes.hasCrashedInLastSession();
+    if (didCrash) {
+      alert('Sorry about that last crash mate!');
+    }
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Button title="Crash" onPress={() => Crashes.generateTestCrash()} />
+        <Button
+          title="Calculate Inflation"
+          onPress={() => Analytics.trackEvent('INFLATION CALC')}
+        />
       </View>
     );
   }
