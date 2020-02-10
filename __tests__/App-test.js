a2c83e5bf05a6e4339f9dd8b3e52e12b071788b1
gustavo souto <gustavo.souto@rethink.dev>
2020-02-10 13:34:35 -0300

    jest test initial setup

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -9,6 +9,15 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('appcenter-crashes', () => {
+  return {
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    inFocusDisplaying: jest.fn(),
+  };
+});
+
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const component = renderer.create(<App />);
+  expect(component).toMatchSnapshot();
 });
